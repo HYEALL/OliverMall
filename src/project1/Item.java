dbcb2737148171ad591e88f051f0f7df1c9af4b1
HYEALL <kwakse0720@gmail.com>
2024-05-08 15:26:10 +0900

    로그인 페이지 추가

--- src/project1/Item.java
+++
@@ -1,9 +0,0 @@
-package project1;
-
-public interface Item {
-	public String getName();
-	public double getPrice();
-	public double getDiscount();
-	public int addToCart(CartDTO dto);
-	public void purchase();
-}
